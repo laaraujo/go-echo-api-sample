0a2711cd5f5f79f06693217311a721e325744e7e
lautaro <lautaroarielaraujo@gmail.com>
2024-03-20 00:27:36 -0300

    add logs and better responses to exercise handlers

--- handler/exercises.go
+++ handler/exercises.go
@@ -38,6 +38,7 @@ func (app *App) CreateExercise(c echo.Context) error {
 func (app *App) GetExercise(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
+		log.Println(err)
 		return echo.NewHTTPError(http.StatusBadRequest, "Invalid Exercise ID")
 	}
 	cc := c.Request().Context()
@@ -52,22 +53,26 @@ func (app *App) GetExercise(c echo.Context) error {
 func (app *App) UpdateExercise(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
+		log.Println(err)
 		return echo.NewHTTPError(http.StatusBadRequest, "Invalid Exercise ID")
 	}
 
 	cc := c.Request().Context()
 
 	if _, err := app.Queries.GetExercise(cc, int32(id)); err != nil {
-		return echo.NewHTTPError(http.StatusNotFound)
+		log.Println(err)
+		return echo.NewHTTPError(http.StatusNotFound, "Exercise not found")
 	}
 
 	exercise := new(db.UpdateExerciseParams)
 	exercise.ID = int32(id)
 	if err := c.Bind(exercise); err != nil {
+		log.Println(err)
 		return echo.NewHTTPError(http.StatusBadRequest)
 	}
 
 	if err := app.Queries.UpdateExercise(cc, *exercise); err != nil {
+		log.Println(err)
 		return echo.NewHTTPError(http.StatusInternalServerError)
 	}
 
@@ -77,6 +82,7 @@ func (app *App) UpdateExercise(c echo.Context) error {
 func (app *App) DeleteExercise(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
+		log.Println(err)
 		return echo.NewHTTPError(http.StatusBadRequest, "Invalid Exercise ID")
 	}
 
@@ -84,7 +90,7 @@ func (app *App) DeleteExercise(c echo.Context) error {
 
 	if err := app.Queries.DeleteExercise(cc, int32(id)); err != nil {
 		log.Println(err)
-		return echo.NewHTTPError(http.StatusNotFound)
+		return echo.NewHTTPError(http.StatusInternalServerError)
 	}
 	return c.JSON(http.StatusOK, "")
 }
